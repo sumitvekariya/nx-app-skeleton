38462e00909a33e754f709b41624430f5c933dff
Sumit Vekariya <sumitvekariya7@gmail.com>
2018-12-12 21:34:15 +0530

    node BE configured

---
+++ apps/family-tree/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-app-skeleton-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'family-tree';
+}
